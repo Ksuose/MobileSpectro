a6d982cbfd6b618dbebe0e13859005b906d9628a
Ksuose <erenbugra09@gmail.com>
2025-11-17 23:39:29 +0300

    aaah

--- backend/main_v2.py
+++ backend/main_v2.py
@@ -228,5 +228,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="192.168.1.142", port=port)
+    # This runs the app locally when you press "play" in VSCode
+    uvicorn.run(app, host="127.0.0.1", port=8000)
